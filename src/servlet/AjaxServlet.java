1cfe0e27a7531673eed601a0b01071ec396d31ab
Vic Liu <liuchenwei2000@163.com>
2015-06-25 11:48:11 +0800

    add Ajax

---
+++ src/servlet/AjaxServlet.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * ≤‚ ‘Ajax«Î«Ûµƒservlet
+ */
+public class AjaxServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		PrintWriter writer = resp.getWriter();
+		writer.println("this is a text from server for Ajax testing.");
+		writer.flush();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		PrintWriter writer = resp.getWriter();
+		writer.println("Your name is " + req.getParameter("name") + "<br>");
+		writer.println("Your age is " + req.getParameter("age"));
+		writer.flush();
+	}
+}
